7fb58f571994012ea220181e64af0c3bdd93f7e4
Luisnefelibato <luisfgomezosp@gmail.com>
2025-04-29 18:58:36 -0500

    MEjor

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+    content: [
+      "./index.html",
+      "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+      extend: {
+        colors: {
+          primary: "#0f172a",
+          secondary: "#1e293b",
+          accent: "#38bdf8",
+        },
+        backgroundImage: {
+          "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+          "gradient-conic": "conic-gradient(var(--tw-gradient-stops))",
+        },
+      },
+    },
+    plugins: [],
+  }
